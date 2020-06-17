ef66e9e343164d9103b0a97c64ea7b30789493b7
zailushang0501 <zailushang0501@163.com>
2020-06-17 19:01:35 +0800

    用一维数组去描述二维数组

--- 5/listMap.cpp
+++ 5/listMap.cpp
@@ -1,61 +1,71 @@
 #include "listMap.h"
 
-template<typename T> void listMap<T>::insert(int i,int index,const T &theElement){    
-    int size = last[i]-front[i];//计算出该线性表有多少个元素
-    bool rightFullFlag=0,leftFullFlag=0;
+template<typename T> void listMap<T>::insert(int i,int index,const T &theElement){
+    bool rightFullFlag = 0,leftFullFlag = 0;
+    int listSize = last[i] - front[i];//首先得到数组的大小
+    //判读索引是不是有效的
+    if(index < 0 || index > listSize)//你要插入的元素的索引不能小于0也不能超过当前数组的大小
+        throw illeagleParameter("the index must be right");
+    //判断完索引以后，我们需要去插入数据了
     /*
-     1.先检查下标是不是正确的；
-     2.判断这个线性表还有没有空间
-     */
-    if(index > size)//下标不符合选项
-        throw illeagleParameter("index must be right");
-    if(last[i] != front[i+1]){//如果数组还有空间的话
-        //把index后面的元素拷贝到index+1后的位置
-        std::copy(element+front[i]+index+1,element+last[i],element+front[i]+index+2);
-        element[front[i]+index] = theElement;
-        last[i]++;//将元素的长度加1。
-    }
-    else{//如果内存满了，那么首先查找右边有没有内存空间
-        int j = i+1;
-        while(last[j] == front[j+1]){//如果右边的第n个列表内存空间满了
-            if(last[j] == length-1){//到末尾了
+    1.第一步首先判断本线性列表的空间有没有满
+    */
+    if(last[i] != front[i+1]){//如果当前这个数组还有空间
+        //copy_back是将(first,last)元素拷贝到第三个迭代器指向的位置以及前边的位置，详情请看源码
+        //所以这样可以很好的避免-1这个下标的处理(-1,last[i]]的元素拷贝
+        //注意，这个copy_backward算法中的last表示的是尾后指针。所以需要last[i]+1;
+        std::copy_backward(element+front[i]+index,element+last[i]+1,element+(last[i]+2));//从last[i]+1这个位置开始倒叙拷贝
+        //拷贝完以后，我们需要插入数据，以及更改尾部指针的索引值
+        element[front[i]+index+1] = theElement;
+        last[i]++;
+    }else{//如果内存空间已经被利用完了的话，那么先搜索右边的空间有没有有剩余的
+        int j = i+1;//现在j为i的右边的第一个数组
+        while(last[j] == front[j+1]){//如果右边的数组空间也被利用完了
+            //那么就继续向右边开始搜索,但是搜索之前要注意一点，先判断有没有到最右边的终点
+            if(last[j] == length-1)//如果到了终点，好的，退出，然后往左查找
+            {
                 rightFullFlag = 1;
-                break;
+                break;//退出，开始从i的左端搜索
             }
-            j++;
+            j++;//如果没有达到终点的话，我们继续向右查找
         }
-        if(rightFullFlag == 1){//如果右边满了
-            j = i-1;
+        if(rightFullFlag == 0){//如果右边有空间，
+            std::copy_backward(element+front[i]+index,element+last[j]+1,element+last[j]+2);
+            //接下来需要把转移的数组进行下标的一个变换，因为位置发生变换了
+            //首先i的尾元素的指针的索引值要加1
+            element[front[i]+index+1] = theElement;
+            last[i]++;
+            //剩下的他们的首前和尾部指针的索引值都加1就好了，也就是顺移
+            for(int k = i+1;k <= j;k++){
+                front[k]++;last[k]++;
+            }
+        }else{//如果右边满了的话，那好，去左边找新的内存空间
+            j = i-1;//j为i左边第一个数组
             while(last[j] == front[j+1]){
-                if(j == 0){//到最左边了
-                    leftFullFlag = 1;
+                if(last[j] == -1){
+                    leftFullFlag=1;//左边也满了，这个时候报错就好了
+                    std::cout<<"\nthere is no space to insert"<<std::endl;
                 }
-                j--;
+                j--;//往左边找数组
             }
-            if(leftFullFlag==1){
-                std::cout<<"all full\n";
-            }else{//如果左边没有满
-                std::copy(element+front[i],element+front[i]+index,element+front[j+1]-1);
-                element[front[i]+index] = theElement;
-                for(int k = i;k != j;--k){//修改各个序列的索引起始下标
-                    front[k]--;//i的起始下标要减一，因为左移了
-                    last[k]--;
+            if(leftFullFlag==0){
+                std::copy(element+front[j]+1,element+front[i]+index+2,element+front[j+1]);
+                //改变一些front和last的值
+                //首先改变front[i]的值
+                front[i]--;
+                //接着顺移一些front和last的值
+                for(int k = i-1;k != j;--k){
+                    last[k]--;//往左移动一下
+                    front[k]--;//往左移动一下
                 }
-                
-            }
-        }else{
-        //如果右边没有满
-            std::copy(element+front[i]+index+1,element+last[j],element+front[i]+index+2);
-            element[front[i]+index] = theElement;
-            for(int k = i;k != j;++k){
-                last[k]++;//第一个last要加一；
-                front[k+1]++;//第二个front肯定要加一
             }
         }
     }
 }
 template<typename T> T listMap<T>::get(int i,int index)const{
-    return element[front[i]+index];
+    return element[front[i]+index+1];//因为front[i]表示的第一个元素的
+    //前一个位置，所以查找元素的时候需要加1。
+
 }
 template<typename T> int listMap<T>::capacity()const{
     return length;
@@ -68,28 +78,48 @@ template<typename T> int listMap<T>::getFront(int i)const{
 template<typename T> int listMap<T>::getLast(int i)const{
     return last[i];
 }
+template<typename T> void listMap<T>::printAllMemeber()const{
+    for(int i = 0; i != length; ++i)
+        std::cout<<element[i]<<",";
+}
 int main()
 {
-    listMap<int>a(5,5);
-    for(int i = 1; i != 6;i++){
-        std::cout<<"the front["<<i<<"] is "<<a.getFront(i)
-        <<" and the last[i] is "<<a.getLast(i)<<std::endl;
+    listMap<int> a(5,5);
+    /*
+    std::cout<<"the inist is\n";
+    for(int i = 1; i <= 5;++i)
+        std::cout<<"font["<<i<<"] is "<<a.getFront(i)<<" and last["
+        <<i<<"]"<<" is "<<a.getLast(i)<<std::endl;
+    std::cout<<std::endl;
+    */
+    for(int i = 0;i != 5;++i){
+        a.insert(1,i,i*10+1);
+        a.insert(2,i,i*10+2);
+        a.insert(3,i,i*10+3);
+        a.insert(4,i,i*10+4);
+    }   
+    for(int i = 1; i <= 5;++i)
+        std::cout<<"font["<<i<<"] is "<<a.getFront(i)<<" and last["
+        <<i<<"]"<<" is "<<a.getLast(i)<<std::endl;
+    for(int i = 0;i != 5;++i)
+        std::cout<<"get(1,"<<i<<") is "<<a.get(1,i)<<std::endl;
+    std::cout<<std::endl;
+    for(int i = 0;i != 5;++i){
+        std::cout<<"get(2,"<<i<<") is "<<a.get(2,i)<<std::endl;
     }
-    std::cout<<"the length is "<<a.capacity()<<std::endl;
-    for(std::size_t i = 0; i != 5; ++i){
-        a.insert(2,i,i*10);
-        a.insert(3,i,i*10+1);
-        a.insert(4,i,i*10+2);
+    std::cout<<"\nafter the operator a.insert(1,0,100) the a is\n";
+    a.insert(3,0,100);
+    for(int i = 1; i <= 5;++i)
+        std::cout<<"font["<<i<<"] is "<<a.getFront(i)<<" and last["
+        <<i<<"]"<<" is "<<a.getLast(i)<<std::endl;
+    a.printAllMemeber();
+    for(int i = 0;i != 5;++i)
+        std::cout<<"get(1,"<<i<<") is "<<a.get(1,i)<<std::endl;
+    std::cout<<std::endl;
+    for(int i = 0;i != 5;++i){
+        std::cout<<"get(2,"<<i<<") is "<<a.get(2,i)<<std::endl;
     }
-    a.insert(3,5,500);
-    std::cout<<"a[3] is ";
-    for(int i = 0; i != 6; ++i){
-        std::cout<<a.get(3,i)<<",";
-    }
-    for(int i = 1; i != 6;i++){
-        std::cout<<"the front["<<i<<"] is "<<a.getFront(i)
-        <<" and the last[i] is "<<a.getLast(i)<<std::endl;
-    }
+
     return 0;
 }
 
--- 5/listMap.h
+++ 5/listMap.h
@@ -13,8 +13,18 @@ public:
 template<typename T> class listMap
 {
 public:
+    //类的构造函数初始化顺序:
+    //构造函数初始值列表只说明用于初始化成员的值，而不限定初始化的具体执行顺序，成员
+    //的初始化顺序与它们在类定义中的出现顺序一致：第一个定义的成员先被初始化，然后第二个
+    //以此类推，构造函数初始值列表中初始值的前后位置关系不会影响实际的初始化顺序，
+    //所以在下面的初始化的时候，element是先于length，如果你要使用element(new T[length])
+    //那么就会出问题，因为length这个时候还没被初始化，所以你的length值不是listSize*elementSize!!!
+    //注意，在参数初始化的顺序，如果不清楚，就不要去写element(new T[listSize*element])这种
+    //因为如果你不确定length和element谁先被初始化，如果length先被初始化是没问题的，但是
+    //如果element先被初始化就完蛋了，因为此时的length是一个不确定的值，所以这个时候要注意
+    //为了以防万一，我们就不使用不确定的元素去初始化，直接使用已知的。
     listMap(int listSize=1000,int elementSize=1000):front(new T[listSize+2]),last(new T[listSize+2]),
-    length(listSize * elementSize),element(new T[length]){
+    length(listSize * elementSize),element(new T[listSize*elementSize]){
         front[0] = last[0] = -1;
         front[listSize+1] = last[listSize+1] = length - 1;
         //放两个边界主要是为了好处理，左右内存空间还有没有空的问题，
@@ -24,6 +34,8 @@ public:
         //相等的。
         for(int i = 1; i <= listSize;++i){
             front[i] = last[i] = -1+elementSize*(i-1);
+        for(int i = 0; i != length; ++i)
+            element[i] = 0;
         }
     }
     void insert(int i,int index,const T &theElement);//在第i个表的index处插入元素theEleme
@@ -31,11 +43,12 @@ public:
     int capacity()const;
     int getFront(int i)const;
     int getLast(int i)const;
+    void printAllMemeber()const;
     ~listMap() {}
 
 private:
-    T *front;//front和last中存放的值是每个列表中第一个元素的前边位置，以及
-    T *last;//最后一个元素的位置
+    T *front;//表示每个线性数组的第一个元素的前面的位置(最开始元素的前一个位置)
+    T *last;//表示每一个线性数组的最后一个元素的位置(索引)
     T *element;//存放所有元素的指针，每个数组中存放的大小是lastSize大小的
     int length;
 };
